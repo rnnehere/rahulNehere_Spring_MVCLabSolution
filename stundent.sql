2908291fea191dd2d56106eeb78466b4fe8b1b78
Rahul <rnnehere@gmail.com>
2022-03-24 10:38:14 +0530

    db

---
+++ stundent.sql
@@ -0,0 +1,12 @@
+create database studentdatabase;
+use studentdatabase;
+create table student
+(
+id int primary key auto_increment,
+name varchar(50),
+department varchar(50),
+country varchar(50)
+);
+use studentdatabase;
+insert into student(name,department,country)
+values ('Monika','E&TC','INDIA');
